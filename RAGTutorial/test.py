25a860d1a9323681d7919a084afcb25df252c26e
Eddie R Huang <erh50@duke.edu>
2024-08-05 14:09:18 +0100

    Tutorial, some problems are that I need to subscribe to OpenAI to increase the amount they can encode, and/or find a way to use sentenceTransformer instead

--- RAGTutorial/test.py
+++ RAGTutorial/test.py
@@ -1,5 +1,15 @@
 import pandas as pd
 import csv
+from dotenv import load_dotenv, dotenv_values
+import os
+from langchain.document_loaders import UnstructuredPDFLoader, OnlinePDFLoader, PyPDFLoader, TextLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_openai import OpenAIEmbeddings
+from pinecone import Pinecone, ServerlessSpec
+from sentence_transformers import SentenceTransformer
+
+model = SentenceTransformer('all-MiniLM-L6-v2')
+
 
 # data = pd.read_csv('/Users/eddie/VSC/RAGExpirement/RAGTutorial/chatgpt_reviews.csv')
 # data = data.drop(columns=['reviewId','userName','at','appVersion','score','thumbsUpCount','reviewCreatedVersion'])
@@ -14,8 +24,109 @@ import csv
 #     # file using reader object 
 #     for row in reader_obj: 
 #         print(row)
-hi = ''
-data = [[1,2],[3,4],[5,6]]
-hi = data
+# hi = ''
+# data = [[1,2],[3,4],[5,6]]
+# hi = data
 
-print(len(hi))
+# print(len(hi))
+
+load_dotenv()
+
+
+# print(os.getenv('MY_KEY'))
+loader = TextLoader(file_path='/Users/eddie/VSC/RAGExpirement/RAGTutorial/txt/vb.txt')
+# cambridge = PyPDFLoader("/Users/eddie/VSC/RAGExpirement/RAGTutorial/PDF/Cambridge - Wikipedia.pdf")
+# oxford = PyPDFLoader("/Users/eddie/VSC/RAGExpirement/RAGTutorial/PDF/Oxford - Wikipedia.pdf")
+# earth = PyPDFLoader("/Users/eddie/VSC/RAGExpirement/RAGTutorial/PDF/Earth - Wikipedia.pdf")
+# python = PyPDFLoader("/Users/eddie/VSC/RAGExpirement/RAGTutorial/PDF/Python (programming language) - Wikipedia.pdf")
+
+data = loader.load()
+
+print (f'You have {len(data)} document(s) in your data')
+print (f'There are {len(data[0].page_content)} characters in your sample document')
+print (f'Here is a sample: {data[0].page_content[:200]}')
+
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=50)
+texts = text_splitter.split_documents(data)
+
+print (f'Now you have {len(texts)} documents')
+
+####
+
+# api_key = os.environ.get(os.getenv('MY_KEY')) or os.getenv('MY_KEY')
+# pc = Pinecone(api_key=api_key)
+
+# spec = ServerlessSpec(cloud='aws', region= 'us-east-1')
+# index_name = 'cambridge'
+
+# if index_name not in pc.list_indexes().names():
+#     dimensions = 384
+#     pc.create_index(
+#     name=index_name,
+#     dimension=dimensions,
+#     metric="cosine",
+#     spec=spec
+#     )
+
+
+# index = pc.Index(index_name)
+
+# vectors = model.encode(texts)
+# index.upsert(vectors=zip(["doc_" + str(i) for i in range(len(vectors))], vectors))
+
+# # Query embedding and similarity search
+# query = input('What university is Cambridge known for housing?')
+# query_vector = model.encode([query])
+# results = index.query(vector=query_vector.tolist(), top_k=1, include_metadata=True)
+
+# # Retrieve and process the results
+# try:
+#     retrieved_info = [texts[int(result['id'].split("_")[1])] for result in results['matches']]
+#     context = " ".join(retrieved_info)
+
+#     def generate_response(query, context):
+#         return f"Context: {context}"
+
+#     # Generate and return the response
+#     response = generate_response(query, context)
+#     if(context != " "):
+#         print(response)
+#     else:
+#         print("no match")
+# except:
+#     print('No matches found')
+
+OPENAI_API_KEY = os.getenv('OPENAI_Key')
+embeddings = OpenAIEmbeddings(openai_api_key=OPENAI_API_KEY)
+
+from langchain.vectorstores import Chroma
+
+vectorstore = Chroma.from_documents(texts, embeddings)
+
+query = "Cambridge is well known as the city that is the home of which college?"
+docs = vectorstore.similarity_search(query)
+
+for doc in docs:
+    print (f"{doc.page_content}\n")
+
+
+# initialize pinecone
+pinecone_api_key = os.environ.get(os.getenv('MY_KEY')) or os.getenv('MY_KEY')
+pc = Pinecone(api_key=pinecone_api_key)
+
+index_name = 'langchain'
+spec = ServerlessSpec(cloud='aws', region= 'us-east-1')
+dimensions = 384
+
+if index_name not in pc.list_indexes().names():
+    dimensions = 384
+    pc.create_index(
+        name=index_name,
+        dimension=dimensions,
+        metric="cosine",
+        spec=spec
+    )
+
+index = pc.Index(index_name)
+
+docsearch = index.from_texts([t.page_content for t in texts], embeddings, index_name=index_name)
--- RAGTutorial/vectorRag.py
+++ RAGTutorial/vectorRag.py
@@ -1,10 +1,15 @@
 from sentence_transformers import SentenceTransformer
-import pinecone
+from langchain.document_loaders import UnstructuredPDFLoader, OnlinePDFLoader, PyPDFLoader, TextLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 from pinecone import Pinecone
 import os
 from pinecone import ServerlessSpec, PodSpec
 import pandas as pd
 import time
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Initialize the embedding model
 model = SentenceTransformer('all-MiniLM-L6-v2')
@@ -13,7 +18,7 @@ index_name = "test"
 ####
 use_severless = True
 
-api_key = os.environ.get('d52a0a0f-20b9-4a19-be54-e64daeeacc17') or 'd52a0a0f-20b9-4a19-be54-e64daeeacc17'
+api_key = os.environ.get(os.getenv('MY_KEY')) or os.getenv('MY_KEY')
 pc = Pinecone(api_key=api_key)
 
 if use_severless:
